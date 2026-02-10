2a92979a58551da1886525a0815de65ff1b59687
Juwon Lee <juwonlee3465@gmail.com>
2026-02-10 19:50:11 +0800

    PORT unlimited

--- src/coinone_order.js
+++ src/coinone_order.js
@@ -172,6 +172,8 @@ app.get('/api/ticker', async (req, res) => {
     }
 });
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
     console.log('🚀 Coinone Secure Proxy on http://localhost:4000');
 });
